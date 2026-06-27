9719f28d5c75998f6fc943e9db06bba32e0d19fa
Glyphack <sh.hooshyari@gmail.com>
2026-06-27 16:35:02 +0200

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+use embassy_rp::{peripherals::USB, usb};
+
+use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_rp::{
@@ -23,17 +26,31 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
+embassy_rp::bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => usb::InterruptHandler<USB>;
+});
+#[embassy_executor::task]
+async fn logger_task(usb: embassy_rp::Peri<'static, embassy_rp::peripherals::USB>) {
+    let driver = embassy_rp::usb::Driver::new(usb, Irqs);
+
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    spawner.spawn(logger_task(p.USB).unwrap());
+
     let _en = Output::new(p.PIN_2, Level::Low);
     let _dir = Output::new(p.PIN_3, Level::High);
     let mut step = Output::new(p.PIN_26, Level::High);
 
     loop {
         step.set_low();
-        Timer::after_micros(1).await;
+        Timer::after_micros(1 * 10000).await;
+        log::info!("hi step is high: {}", step.is_set_high());
         step.set_high();
-        Timer::after_micros(625).await;
+        Timer::after_micros(625 * 1000).await;
+        log::info!("hi step is high: {}", step.is_set_high());
     }
 }
